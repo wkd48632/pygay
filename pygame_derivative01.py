16d891257c1ba4d167f4dee2acc8d63429cdf769
wkd48632 <wkd48632@gmail.com>
2026-07-06 07:59:00 +0900

    Update pygame_derivative01.py

--- pygame_derivative01.py
+++ pygame_derivative01.py
@@ -4,3 +4,13 @@ os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 
 import pygame
 import numpy
+
+screen = None
+_screen_width = 0
+_screen_height= 0
+
+def make_screen(x=_screen_width,y=_screen_height):
+  screen = pygame.display.set_mode((x, y))
+
+def full_screen():
+  screen = pygame.display.set_mode((screen_width, screen_height))
